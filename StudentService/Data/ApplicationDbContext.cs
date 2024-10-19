50b485631ebc9aadac84f68c7f5d819c27c9c061
Truc4p <truc4pham@gmail.com>
2024-10-19 19:32:44 +0700

    Initial commit

---
+++ StudentService/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using StudentService.Models;
+using System.Collections.Generic;
+
+namespace StudentService.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
